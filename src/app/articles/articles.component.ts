2f71dacc67fad16729d1dcaca00b5c3375b366ba
d3meter <mark.demeter3@gmail.com>
2023-04-22 17:11:07 +0200

    add filter articles

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Article } from './article.model';
 import { ArticlesService } from './articles.service';
 
@@ -11,13 +11,62 @@ export class ArticlesComponent implements OnInit {
   articles: Article[];
   selectedArticle: Article = null;
   displayValue = 'none';
+  @Input() username: string;
 
   constructor(private articlesService: ArticlesService) {}
+
+  /*   ngOnInit() {
+    this.articlesService.getArticles().subscribe(
+      (response) => {
+        this.articles = response;
+        console.log(response);
+      },
+      (error: string) => {
+        this.articlesService.errorMessage.next(error);
+      }
+    }
+    ); */
+
+  /*   ngOnInit() {
+    this.articlesService.getArticles().subscribe(
+      (response) => {
+        const username = "SuperTramp"; // define the username to filter
+        this.articles = response.filter((article) => article.author.username === username);
+        console.log(response);
+      },
+      (error: string) => {
+        this.articlesService.errorMessage.next(error);
+      }
+    );
+  } */
+
+  /*   ngOnInit() {
+    const username = null; // define the username to filter, or set to null to get all articles
+    this.articlesService.getArticles().subscribe(
+      (response) => {
+        if (username === null) {
+          this.articles = response;
+        } else {
+          this.articles = response.filter((article) => article.author.username === username);
+        }
+        console.log(response);
+      },
+      (error: string) => {
+        this.articlesService.errorMessage.next(error);
+      }
+    );
+  } */
 
   ngOnInit() {
     this.articlesService.getArticles().subscribe(
       (response) => {
-        this.articles = response;
+        if (this.username === null) {
+          this.articles = response;
+        } else {
+          this.articles = response.filter(
+            (article) => article.author.username === this.username
+          );
+        }
         console.log(response);
       },
       (error: string) => {
